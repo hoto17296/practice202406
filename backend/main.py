15362ba9e7730bf1a99dc7cf1fd85159933c6a9b
Yuki Ishikawa <hoto@y32.jp>
2024-06-06 01:23:18 +0000

    enable type check

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,19 @@ from os import getenv
 
 import asyncpg
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class GetCounterResponse(BaseModel):
+    count: int
+
+
 @app.get("/counter")
-async def get_counter():
+async def get_counter() -> GetCounterResponse:
     conn = await asyncpg.connect(getenv("DATABASE_URL"))
     await conn.execute("INSERT INTO access_log DEFAULT VALUES")
-    count = await conn.fetchval("SELECT COUNT(1) FROM access_log")
+    count: int = await conn.fetchval("SELECT COUNT(1) FROM access_log")
     conn.close()
-    return {"count": count}
+    return GetCounterResponse(count=count)
