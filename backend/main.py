bc776bce796093894d75a3ab131ef450808b19c2
Yuki Ishikawa <hoto@y32.jp>
2024-06-06 00:35:54 +0000

    add postgres

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
+from os import getenv
 from fastapi import FastAPI
+import asyncpg
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/counter")
+async def get_counter():
+    conn = await asyncpg.connect(getenv("DATABASE_URL"))
+    await conn.execute("INSERT INTO access_log DEFAULT VALUES")
+    count = await conn.fetchval("SELECT COUNT(1) FROM access_log")
+    conn.close()
+    return {"count": count}
